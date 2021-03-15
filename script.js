ef32f7175a038ae83b84d1dbb66d34e9850ce2b1
Samia Ferdous <samia.ferdous@icloud.com>
2021-03-14 23:27:22 -0400

    adding variables

--- script.js
+++ script.js
@@ -7,7 +7,25 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  var length = prompt("How many characters would you like your password to be? Please enter between 8 to 128 characters.")
+
+  if (length < 8 || plength > 128){
+      alert("The length of your password must be between 8 to 128 characters.")
+  }
   
+  if (length >= 8 && plength <= 128){
+  var cuppercase = confirm("Would you like letters in UPPER CAPS?")
+  var clowercase = confirm("Would you like letters in LOWER CAPS?")
+  var cnumbers = confirm("Do you need numbers in your password?")
+  var csymbols = confirm("Do you need special characters?")
+  }    
+  
+  if (cuppercase != true && clowercase != true && cnumbers != true && csymbols != true){
+      alert("You must select at least one character type!")
+  }
+  
+ 
 
 }
 
